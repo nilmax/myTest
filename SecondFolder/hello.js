c4b54464556a7ea80e69339e3a065d11a9547c91
Nilmax Moura <nilmax@gmail.com>
2015-07-02 16:06:07 -0700

    Create hello.js

---
+++ SecondFolder/hello.js
@@ -0,0 +1,6 @@
+// Hello in Javascript
+
+var who = window.prompt("What is your name");
+var favcolor = window.prompt("What is your favorite color");
+document.write("Hello " + who);
+document.bgColor = favcolor;
